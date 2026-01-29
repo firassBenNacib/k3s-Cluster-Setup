7bf155ae11ee5eca8588aa21d41690b4c215547b
FirasBenNacib <128628709+firassBenNacib@users.noreply.github.com>
2026-01-29 11:52:20 +0100

    Initialize cancel flag on module load

--- src/k3s-cluster-setup/Private/Init.ps1
+++ src/k3s-cluster-setup/Private/Init.ps1
@@ -2,5 +2,6 @@ $script:CancelRequested = $false
 $script:CancelHintShown = $false
 $script:MultipassWaitReadyUnsupported = $false
 $script:MultipassWaitReadyWarned = $false
+$global:K3S_CLUSTER_SETUP_CANCEL_REQUESTED = $false
 
 Set-Variable -Scope Script -Name ScriptName -Value 'k3s-cluster-setup.ps1'
